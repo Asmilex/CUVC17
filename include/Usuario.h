3a0028eab3a8b7467f87cb05cead19a78797436d
Asmilex <andresmm@outlook.com>
2018-06-26 23:17:34 +0200

    changes to class definition

--- include/Usuario.h
+++ include/Usuario.h
@@ -13,10 +13,16 @@ struct Fecha{
 
 class Usuario{
 private:
-	std::string Nombre;
-	int tipo_suscripcion;
-	Fecha limite;
+	std::string _nombre;
+	int _tipo_suscripcion;
+	Fecha _limite;
+
 public:
+	Usuario();
+	Usuario(std::string name, int suscripcion, Fecha limit);
+	Usuario(std::string name, int suscripcion);
+
+	////////////////////////////////////////////////////////////////////
 	/**
 	 * @brief: Devuelve el nombre del usuario
 	 * @return: nombre del usuario
@@ -33,6 +39,35 @@ public:
 	 */
 	int GetTipoSuscripcion() const;
 
+	////////////////////////////////////////////////////////////////////
+	/**
+	 * @brief: modifica el nombre de usuario
+	 */
+	void setName();
+	
+	/**
+	 * @brief: modifica el tipo de suscripción
+	 */
+	void setSuscripcion();
+
+	/**
+	 * @brief: modifica la fecha limite
+	 */
+	void setFechaLimite();
+
+	////////////////////////////////////////////////////////////////////
+	/**
+	 * @brief: mejora la suscripcion y actualiza la fecha límite
+	 */
+	void ActualizarSuscripcion(int suscription);
+
 };
+
+
+/**
+ * @brief: Añade un usuario a la lista de activos
+ * @return: true si ha funcionado, else false
+ */
+bool AnadirUsuario(Usuario *& VIPs);
 
 #endif
--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -1,3 +1,59 @@
-#include "Usuario.h"
+#include <time.h>
+#include "include/Usuario.h"
 
 using namespace std;
+	Usuario::Usuario(){
+
+
+    }
+	Usuario::Usuario(std::string name, int suscripcion, Fecha limit){
+
+
+    }
+	Usuario::Usuario(std::string name, int suscripcion){
+
+
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
+    string Usuario::GetName() const{
+
+    }
+	Fecha Usuario::GetFechaLimite() const{
+
+
+    }
+	int Usuario::GetTipoSuscripcion() const{
+
+
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
+	void Usuario::setName(){
+
+
+    }
+	void Usuario::setSuscripcion(){
+
+
+    }
+	void Usuario::setFechaLimite(){
+
+
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
+	void Usuario::ActualizarSuscripcion(int suscription){
+
+
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
+    bool AnadirUsuario(Usuario *& VIPs){
+
+
+    }
