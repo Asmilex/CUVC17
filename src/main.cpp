269140d167b2ae2c66a4e0c638590fdc9a7b3db8
Jorge <jmc00133@red.ujaen.es>
2018-06-28 12:38:15 +0200

    abrir archivo 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include "UsuariosVIP.h"
+#include <fstream>
 
 using namespace std;
 
+//TODO
+int recuperaMutantesCSV(Mutante* v[], unsigned int tamv, std::string nomArchivo) {
+    std::ifstream fichero;
+    std::string linea;
+    fichero.open(nomArchivo.c_str());
+    if(fichero.good()){
+        for(int i = 0; i < tamv; i++){
+            std::getline(fichero, linea);
+            if(fichero.eof()) break;
+            v[i] = new Mutante();
+            v[i]->fromCSV(linea);
+            
+        }
+        fichero.close();
+        
+    }
+    
+}
 
 int main(){
     int opcion;
+    
+    
     
     do{
         cout << "1. Mostrar Usuarios" << endl();
@@ -17,7 +38,8 @@ int main(){
         
         switch(opcion){
             case 1:break;
-            case 2:break;
+            case 2:
+                break;
             case 3:break;
             case 4:break;
         }
