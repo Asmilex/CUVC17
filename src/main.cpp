dd7c6c4fa712d599fd1960e0bdf308e0289263c9
Asmilex <32983998+Asmilex@users.noreply.github.com>
2018-07-10 20:57:12 +0200

    Updated main.cpp; removed unnecessary code in order to improve cross platform

--- src/main.cpp
+++ src/main.cpp
@@ -2,144 +2,90 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-//#include <windows.h>
 #include <stdio.h>
 
 using namespace std;
 
-/** Descomenta esta función y el condicional del main para compilar para Windows
- 
-
-BOOL CtrlHandler( 	DWORD fdwCtrlType, 
-					const UsuarioVIP * VIPs, const unsigned int dim)
-{    
-	switch( fdwCtrlType ){ 
-    // Handle the CTRL-C signal. 3
-    case CTRL_C_EVENT: 
-		printf( "Ctrl-C event\n\n" );
-      	Beep( 750, 300 ); 
-      	return( TRUE );
-
-    // CTRL-CLOSE: confirm that the user wants to exit. 
-    case CTRL_CLOSE_EVENT: 
-		Beep( 600, 200 );
-		
-		if (VIPs != nullptr) {
-			SaveFile(VIPs, dim);
-			delete [] VIPs;
-		}
-		
-		return( TRUE ); 
-
-    // Pass other signals to the next handler. 
-    case CTRL_BREAK_EVENT: 
-		Beep( 900, 200 ); 
-		printf( "Ctrl-Break event. Saving and existing \n\n" );
-		SaveFile(VIPs, dim);
-		delete [] VIPs;
-		return FALSE; 
-
-    case CTRL_LOGOFF_EVENT: 
-		Beep( 1000, 200 ); 
-		printf( "Ctrl-Logoff event\n\n" );
-		return FALSE; 
-
-    case CTRL_SHUTDOWN_EVENT: 
-		Beep( 750, 500 ); 
-		printf( "Ctrl-Shutdown event\n\n" );
-		return FALSE; 
-
-    default: 
-      	return FALSE; 
-  } 
-} 
-*/
 int main(){
-    //if( SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE ) )
-	{ 
-		//Recuperción de Usuarios
-		vector<UsuarioVIP> VIPs;
+	//Recuperción de Usuarios
+	vector<UsuarioVIP> VIPs;
 
-		// Tamaño > 0 para evitar crasheos por si el archivo estuviera vacío 
-		if (FileExists() && FileSize() > 0){
-			LoadFile(VIPs);
-		}
+	// Tamaño > 0 para evitar crasheos por si el archivo estuviera vacío 
+	if (FileExists() && FileSize() > 0){
+		LoadFile(VIPs);
+	}
 
-	    cout <<"Bienvenido, admin." <<endl;
-		if (VIPs.size() != 0)
-			AutoEliminador(VIPs);
+	cout <<"Bienvenido, admin." <<endl;
+	if (VIPs.size() != 0)
+		AutoEliminador(VIPs);
 
-		int opcion;    
-		while (true){
-			DisplayUsers(VIPs);
-			
-			cout << "   1) Añadir Usuario" << endl;
-			cout << "   2) Eliminar usuario manualmente"<<endl;
-			cout << "   3) Ordenar por nombre"<<endl;
-			cout << "   4) Ordenar por fecha"<<endl;
-			cout << "   5) Salir" << endl;
-			cin >> opcion;
-			
-			switch(opcion){
-				case 1:{
-					string user;
-					int suscripcion;
+	int opcion;    
+	while (true){
+		DisplayUsers(VIPs);
 
-					cout <<"   Nombre del usuario: ";
-					cin >>user;
+		cout << "   1) Añadir Usuario" << endl;
+		cout << "   2) Eliminar usuario manualmente"<<endl;
+		cout << "   3) Ordenar por nombre"<<endl;
+		cout << "   4) Ordenar por fecha"<<endl;
+		cout << "   5) Salir" << endl;
+		cin >> opcion;
 
-					cout <<"   Tipo de suscripción: ";
-					cin >>suscripcion;
+		switch(opcion){
+			case 1:{
+				string user;
+				int suscripcion;
 
-					UsuarioVIP nuevo_usuario(user, suscripcion);
+				cout <<"   Nombre del usuario: ";
+				cin >>user;
 
-					if(AnadirUsuarioVIP(VIPs, nuevo_usuario))
-						cout <<"\nUsuario añadido correctamente\n";
-					else
-						cerr <<"\nNo se ha podudo añadir el usuario\n";
-				} break;
+				cout <<"   Tipo de suscripción: ";
+				cin >>suscripcion;
+
+				UsuarioVIP nuevo_usuario(user, suscripcion);
+
+				if(AnadirUsuarioVIP(VIPs, nuevo_usuario))
+					cout <<"\nUsuario añadido correctamente\n";
+				else
+					cerr <<"\nNo se ha podudo añadir el usuario\n";
 				
-				case 2:{
-					int opcion;
+				SaveFile(VIPs);
+			} break;
 
-					cout <<"   Introduce el número del usuario a eliminar: ";
-					cin >>opcion;
+			case 2:{
+				int opcion;
 
-					if (opcion > 0 && opcion <= VIPs.size() && EliminarUsuarioVIP(VIPs, opcion))
-						cout <<"\nUsuario eliminado correctamente\n";
-					else
-						cout <<"\nNo se ha podido eliminar el usuario\n";
-				} break;
+				cout <<"   Introduce el número del usuario a eliminar: ";
+				cin >>opcion;
+
+				if (opcion > 0 && opcion <= VIPs.size() && EliminarUsuarioVIP(VIPs, opcion))
+					cout <<"\nUsuario eliminado correctamente\n";
+				else
+					cout <<"\nNo se ha podido eliminar el usuario\n";
 				
-				case 3:{
-					sort(VIPs.begin(), VIPs.end(), sortByName);
-				} break;
-				
-				case 4:{
-					sort(VIPs.begin(), VIPs.end(), sortByDate);
-				} break;
+				SaveFile(VIPs);
+			} break;
+			case 3:{
+				sort(VIPs.begin(), VIPs.end(), sortByName);
+				SaveFile(VIPs);
+			} break;
 
-				case 5:{
-					if (VIPs.size() != 0)
-						SaveFile(VIPs);
-					return 0;
-				}
+			case 4:{
+				sort(VIPs.begin(), VIPs.end(), sortByDate);
+				SaveFile(VIPs);
+			} break;
 
-				default:{
-						cout <<"Pulsa la tecla correcta, mamón: ";
-						cin >>opcion;
-				}
-					break;
+			case 5:{
+				if (VIPs.size() != 0)
+					SaveFile(VIPs);
+				return 0;
 			}
-			
+				
+			default:{
+					cout <<"Pulsa la tecla correcta, mamón: ";
+					cin >>opcion;
+			} break;
 		}
-		//Cuando lo probé, con la siguiente linea funcionaba. Supongo que no pasa nada por comentarla
-		//while( 1 ){ } 
-    }  
-    //else{
-    //    printf( "\nERROR: Could not set control handler"); 
-    //    return 1;
-    //}
+	}
 }
 
 
